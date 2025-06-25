7f0287bf95fcc2359b125b5b9c2b8180901a6c94
suraj <suraj@gmail.com>
2025-06-26 02:14:11 +0530

    added docker files and reverse proxy

--- service_2/app.py
+++ service_2/app.py
@@ -13,5 +13,18 @@ def hello():
     return jsonify(message="Hello from Service 2")
 
 
+@app.route("/")
+def index():
+    return """
+    <h1>Welcome to the Reverse Proxy Demo!</h1>
+    <p>Available endpoints:</p>
+    <ul>
+        <li><a href="http://localhost:8080/service1/ping">Service 1 Ping</a></li>
+        <li><a href="http://localhost:8080/service1/hello">Service 1 Hello</a></li>
+        <li><a href="http://localhost:8080/service2/ping">Service 2 Ping</a></li>
+        <li><a href="http://localhost:8080/service2/hello">Service 2 Hello</a></li>
+    </ul>
+    """
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8002)
